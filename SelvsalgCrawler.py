ea0a17c1bce6bebba46ca9a7043e668897aed73a
mvalvik <morten@valvik.dk>
2014-10-01 19:49:22 +0200

    Updated with config values

--- SelvsalgCrawler.py
+++ SelvsalgCrawler.py
@@ -8,12 +8,13 @@ import shelve
 import hashlib
 import smtplib
 from email.mime.text import MIMEText
+import config as cfg
 
 if __name__ == "__main__":
     reload(sys)
     sys.setdefaultencoding("utf-8")
 
-hipsterZipCodes = [str(x) for x in range(999,2300)]
+hipsterZipCodes = [str(x) for x in range(cfg.selvsalg['hipsterCodesLow'], cfg.selvsalg['hipsterCodesMax'])]
 
 class Residence():
 	pass
@@ -29,13 +30,13 @@ def get_apartment(section_url):
     for residence_card in residence_cards:
     	res = Residence()
     	res.type = str(residence_card.select(".ribbon")[0].contents[0])
-    	res.price = int(str(residence_card.select("td.price")[0].contents[0]).replace(',', '').replace('.','').replace('-',''))
+    	res.price = str(residence_card.select("td.price")[0].contents[0]).replace(',', '').replace('.','').replace('-','')
     	res.ydelse = str(residence_card.select("td.hidden-xs")[0].contents[0])
     	res.description = str(residence_card.select("h6 a")[0].contents[0])
     	res.link = str(section_url + residence_card.select("h6 a")[0].attrs['href'])
     	res.adress = str(residence_card.select(".polaroid p")[0].contents[0])
     	print res
-    	if any(x in res.adress for x in hipsterZipCodes) and res.price < 1200000:
+    	if any(x in res.adress for x in hipsterZipCodes) and int(res.price) < cfg.selvsalg['maxPrice']:
     		residences.append(res)
 
     for residence in residences:
@@ -54,19 +55,19 @@ def get_apartment(section_url):
 
 def sendEmail(residence):
 	FROM = 'selvsalg@gmail.com'
-	TO = ['morten@valvik.com']
+	TO = cfg.selvsalg['emailReceiver']
 	SUBJECT = "Ny bolig på selvsalg: " + residence.description
 	TEXT = "Beskrivelse: " + " Pris: " + residence.price + "  Adresse: " + residence.adress + " Link: " + residence.link
 
 	message = """\From: %s\nTo: %s\nSubject: %s\n\n%s"""%(FROM, ", ".join(TO), SUBJECT, TEXT)
-	username = 'mrvalvik@gmail.com'
-	password = 'hbmsnvwygiuldhhe'
+	username = cfg.selvsalg['emailHostUsername']
+	password = cfg.selvsalg['emailHostPassword']
 
-	server = smtplib.SMTP('smtp.gmail.com:587')
+	server = smtplib.SMTP(cfg.selvsalg['emailHostServer'])
 	server.ehlo()
 	server.starttls()
 	server.login(username,password)
-	server.sendmail('selvsalg@valvik.dk','morten@valvik.dk', message)
+	server.sendmail('selvsalg@valvik.dk',cfg.selvsalg['emailReceiver'], message)
 	server.quit()
 	print 'email sent'
 
